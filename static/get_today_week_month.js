052cbbd4a8d8db2191b37d33d65eebf105107ddf
JayyyXp <henrik.hoppania@gmail.com>
2020-12-08 17:52:06 +0200

    css bs

--- static/get_today_week_month.js
+++ static/get_today_week_month.js
@@ -4,7 +4,6 @@ const year = today.getFullYear();
 
 let onejan = new Date(today.getFullYear(), 0, 1);
 let week = Math.ceil( (((today.getTime() - onejan.getTime()) / 86400000) + onejan.getDay() + 1) / 7 );
-console.log(`${year}-W${week}`);
 document.getElementById('week').value = `${year}-W${week}`;
 
 let month = today.getMonth() + 1;
